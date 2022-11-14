62216a93ed97c184476214ce59938ca1557181ee
oiysful <oiysful@gmail.com>
2022-11-15 00:49:43 +0900

    2022-11-15 1. todo-container 추가 : 로그인 전, 후 표시 유무 변경

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const greeting = document.querySelector("#greeting");
+const todoContainer = document.querySelector("#todo-container");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -15,22 +16,19 @@ function onLoginSubmit(event) {
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
     paintGreetings(username);
+    paintTodoContainer();
 }
 
 function getMessage() {
     const date = new Date();
     const hours = date.getHours();
-    //morning : 05 ~ 12
-    //afternoon : 12 ~ 18
-    //evening : 18 ~ 22
-    //night : 22 ~ 05
     if(hours >= 5 && hours < 12) {
         return "Good Morning!";
     } else if(hours >= 12 && hours < 18) {
         return "Good Afternoon!";
     } else if(hours >= 18 && hours < 22) {
         return "Good Evening.";
-    } else if(hours >= 22 || hours < 5) {
+    } else {
         return "Good Night.";
     }
 }
@@ -40,11 +38,16 @@ function paintGreetings(username) {
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
+function paintTodoContainer() {
+    todoContainer.classList.remove(HIDDEN_CLASSNAME);
+}
+
 if(savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     paintGreetings(savedUsername);
+    paintTodoContainer();
 }
 
 const logoutBtn = document.getElementById("logout");
